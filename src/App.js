e15de2a81cced292e505f1295a1de0aefd6d57da
pwlono <68565696+pwlono@users.noreply.github.com>
2020-09-06 12:21:40 -0300

    services_1erComponente

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
 import React, { useEffect, useState } from "react";
 import "./App.css";
+import getGifs from "./services/getGifs";
+import Gif from "./components/Gif";
 
 function App() {
+  const [gifs, setGifs] = useState([]);
+
+  useEffect(function () {
+    getGifs({ keyword: "aguero" }).then(gifs => setGifs(gifs));
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header"></header>
+    <div className='App'>
+      <section className='App-content'>
+        {gifs.map(singleGif => (
+          <Gif title={singleGif.title} url={singleGif.url} id={singleGif.id} />
+        ))}
+        );
+      </section>
     </div>
   );
 }
